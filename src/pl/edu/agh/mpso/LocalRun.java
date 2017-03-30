342b396b8eb89a72fecc454b6adec64084442f45
Zuzanna Chrzastek <zuzanna.chrzastek@gmail.com>
2017-03-30 18:43:18 +0200

    LocalRun duplications removed

--- src/pl/edu/agh/mpso/LocalRun.java
+++ src/pl/edu/agh/mpso/LocalRun.java
@@ -21,7 +21,9 @@ import pl.edu.agh.mpso.species.SpeciesType;
 import pl.edu.agh.mpso.swarm.MultiSwarm;
 import pl.edu.agh.mpso.swarm.SwarmInformation;
 import pl.edu.agh.mpso.transition.order.DefaultOrderFunction;
+import pl.edu.agh.mpso.transition.order.OrderFunction;
 import pl.edu.agh.mpso.transition.shift.DefaultShiftFunction;
+import pl.edu.agh.mpso.transition.shift.ShiftFunction;
 
 /**
  * 
@@ -34,150 +36,21 @@ import pl.edu.agh.mpso.transition.shift.DefaultShiftFunction;
  * - proportional share of given species
  */
 public class LocalRun {
-	private static final int _7 = 7;
-	private static String className;
-	private static List<Thread> threads;
 
 	public static void main(String[] args) throws InstantiationException, IllegalAccessException, IOException, InterruptedException {
+//		RunUtils runUtils = new RunUtils();
 		FitnessFunction fitnessFunction = new Schwefel();
 		NUMBER_OF_DIMENSIONS = 100;
 		NUMBER_OF_ITERATIONS = 3000;
 		int executions = 30;
 		
-		className = fitnessFunction.getClass().getName();
-		
-		threads = new ArrayList<Thread>();
-		
 //		runParallel(0, fitnessFunction, new int[]{25,0,0,0,0,0,0,0}, executions);
 //		runParallel(0, fitnessFunction, new int[]{0,10,5,5,0,0,0,5}, executions);
 //		runParallel(0, fitnessFunction, new int[]{1,4,0,2,0,0,0,18}, executions);
 //		runParallel(0, fitnessFunction, new int[]{0,5,5,5,0,0,0,10}, executions);
-		runParallel(0, fitnessFunction, new int[]{1,4,0,5,0,0,0,15}, executions);
+		RunUtils.runParallel(0, fitnessFunction, new int[]{1,4,0,5,0,0,0,15}, executions);
 //		runParallel(0, fitnessFunction, new int[]{5,7,6,7,0,0,0,0}, executions);
 //		runParallel(0, fitnessFunction, new int[]{0,6,5,6,1,1,1,5}, executions);
-		
-		for(Thread thread : threads){
-			thread.join();
-		}
 	}
 
-	private static void runParallel(final int id, final FitnessFunction fitnessFunction, final int[] speciesArray, final int executions){
-		Thread thread = new Thread(new Runnable() {
-			
-			public void run() {
-				for(int i = 0; i < executions; i++){
-					try {
-						simulate(fitnessFunction, speciesArray, id, executions, i);
-					} catch (Throwable e) {
-						e.printStackTrace();
-					}
-				}
-			}
-		});
-		
-		threads.add(thread);
-		thread.start();
-	}
-
-	private static void simulate(FitnessFunction fitnessFunction,
-			int[] speciesArray, int id, int executions, int i) throws IOException {
-		SimulationOutput output = null;
-		try{
-			long tic = System.currentTimeMillis();
-			SimulationResult result = run(speciesArray, fitnessFunction);
-			long toc = System.currentTimeMillis();
-			long diff = toc - tic;
-			long seconds = diff / 1000L;
-			long minutes = seconds / 60L;
-			long hours = minutes / 60L;
-			System.out.println("" + id + ": Execution " + (i+1) + " of " + executions);
-			System.out.println("\tDone in: " + hours + " hours " + (minutes%60) + " minutes " + (seconds % 60) + " seconds");
-			
-			output = new SimulationOutputOk();
-			((SimulationOutputOk) output).results = result;
-			SimulationResultDAO.getInstance().writeResult(result);
-			SimulationResultDAO.getInstance().close();
-			System.out.println(result.bestFitness);
-		} catch (Throwable e){
-			e.printStackTrace();
-			output = new SimulationOutputError();
-			((SimulationOutputError)output).reason = e.toString() + ": " + e.getMessage();
-		} finally {
-//			Writer writer = new FileWriter("output.json");
-//			Gson gson = new Gson();
-//			gson.toJson(output, writer);
-//			writer.close();
-		}
-	}
-	
-
-	private static SimulationResult run(int [] particles, FitnessFunction fitnessFunction) {
-		int cnt = 0;
-		List<SwarmInformation> swarmInformations = new ArrayList<SwarmInformation>();
-		
-		for(int i = 0; i < particles.length; i++){
-			if(particles[i] != 0){
-				cnt += particles[i];
-				
-				SpeciesType type = SpeciesType.values()[i];
-				SwarmInformation swarmInformation = new SwarmInformation(particles[i], type);
-				swarmInformations.add(swarmInformation);
-			}
-		}
-		
-		SwarmInformation [] swarmInformationsArray = new SwarmInformation [swarmInformations.size()]; 
-		MultiSwarm multiSwarm = new MultiSwarm(swarmInformations.toArray(swarmInformationsArray), fitnessFunction);
-		
-		Neighborhood neighbourhood = new Neighborhood1D(_7, true);
-		multiSwarm.setNeighborhood(neighbourhood);
-		
-		multiSwarm.setInertia(0.95);
-		multiSwarm.setNeighborhoodIncrement(0.9);
-		multiSwarm.setParticleIncrement(0.9);
-		multiSwarm.setGlobalIncrement(0.9);
-		
-		multiSwarm.setMaxPosition(20);
-		multiSwarm.setMinPosition(-20);
-		
-		multiSwarm.setOrderFunction(new DefaultOrderFunction());
-		multiSwarm.setShiftFunction(new DefaultShiftFunction());
-		
-//		multiSwarm.setAbsMaxVelocity(2.0);
-		multiSwarm.init();
-		
-		List<Double> partial = new ArrayList<Double>(NUMBER_OF_ITERATIONS / 100);
-		
-		for(int i = 0; i < NUMBER_OF_ITERATIONS; ++i) {
-			// Evolve swarm
-			multiSwarm.evolve();
-			
-			//display partial results
-			if(NUMBER_OF_ITERATIONS > 100 && (i % (NUMBER_OF_ITERATIONS / 100) == 0)){
-				partial.add(multiSwarm.getBestFitness());
-			}
-		}
-		
-		//create output.json
-		SimulationResult output = new SimulationResult();
-		output.fitnessFunction = className;
-		output.iterations = NUMBER_OF_ITERATIONS;
-		output.dimensions = NUMBER_OF_DIMENSIONS;
-		output.partial = partial;
-		output.bestFitness = multiSwarm.getBestFitness();
-		output.totalParticles = NUMBER_OF_PARTICLES;
-		
-		output.species1 = particles[0];
-		output.species2 = particles[1];
-		output.species3 = particles[2];
-		output.species4 = particles[3];
-		output.species5 = particles[4];
-		output.species6 = particles[5];
-		output.species7 = particles[6];
-		output.species8 = particles[7];
-		
-		output.orderFunction = multiSwarm.getOrderFunction().getClass().getSimpleName();
-		output.shiftFunction = multiSwarm.getShiftFunction().getClass().getSimpleName();
-		
-		return output;
-	}
 }
